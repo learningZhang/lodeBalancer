f9f6f082eaf51ac3714f331200aad7072fac25f1
zhang <1554458363@qq.com>
2017-06-26 19:06:07 +0800

    因为之前强行合并造成了错乱，现在重新发送2017/6/26

--- lodeBalancer/head.h
+++ lodeBalancer/head.h
@@ -33,8 +33,6 @@ class CMysql
 		bool insertInto_serverfd(int fd, int id);
 		int  get_fd(int id);
 		int  get_id(int fd);
-		int deleteFromStates(char *name);
-		//
 	private:
 		MYSQL *pcon;
 	 	MYSQL_RES *pres;
