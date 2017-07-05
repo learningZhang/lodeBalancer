2798bf0024b9db04a7432ca8009b509967f884cd
zhang <1554458363@qq.com>
2017-07-06 07:48:34 +0800

    服务器端

---
+++ lodeBalancer/server1.cpp
@@ -0,0 +1,195 @@
+#include "head.h"
+//#include "memcached.h"
+
+int main()
+{
+    int listenfd;
+    listenfd = socket(AF_INET, SOCK_STREAM, 0);
+    if(listenfd == -1)
+    {
+        cout<<"listenfd create fail!"<<endl;
+        return -1;
+    }
+    sockaddr_in server;
+    memset(&server, 0, sizeof(sockaddr_in));
+    server.sin_family = AF_INET;
+    server.sin_port = htons(10000);
+    server.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+    if(-1 == bind(listenfd, (sockaddr*)&server, sizeof(server)))
+    {
+        cout<<"listenfd bind fail!"<<endl;
+        return -1;
+    }
+
+    if(-1 == listen(listenfd, 20))
+    {
+        cout<<"listenfd listen fail!"<<endl;
+        return -1;
+    }
+
+    pthread_t tid;
+    struct event_base* base = event_init();
+    struct event *listen_event = event_new(base, listenfd,  EV_READ|EV_PERSIST, ProcListenfd, &tid);
+    event_add( listen_event, NULL );
+
+    cout<<"server started..."<<endl;
+    event_base_dispatch(base);
+    return 0;   
+}
+
+void ProcListenfd(evutil_socket_t fd, short , void *arg)
+{
+    sockaddr_in client;
+    socklen_t len = sizeof(client);
+    int clientfd = accept(fd, (sockaddr*)&client, &len);
+    cout<<"new client connect server! client info:"
+        <<inet_ntoa(client.sin_addr)<<" "<<ntohs(client.sin_port)<<endl;
+
+    pthread_t tid;
+    pthread_create(&tid, NULL, ReadThread, (void*)clientfd);
+}
+
+bool sendMesgFromDb(CMysql &db, int tofd, const char*name, int fd)
+{
+        Json::Value response;
+        char *buff;
+        char *temp=(char *)malloc(MESSAGE_MAX_LENGTH*sizeof(char));
+        if (temp == NULL) return false;
+	memset(temp, 0, MESSAGE_MAX_LENGTH);
+        if (!findMesgByName(db, name, temp, MESSAGE_MAX_LENGTH))
+        {
+                free(temp);
+		temp = NULL;
+                return false;
+        }
+        else
+        {
+                const char *tmp = "-";
+                char *fromuser = strtok(buff, tmp);
+                char *msg = strtok(NULL, tmp);
+
+                response["FD"] = tofd;
+                response["msgtype"] = EN_MSG_ACK;
+                response["msg"] = buff;
+                response["from"] = fromuser;
+
+                int x = send(fd, response.toStyledString().c_str(), strlen(response.toStyledString().c_str()), 0);
+                free(temp);
+		temp = NULL;
+                if (x == -1)
+                {
+                        return false;
+                }
+                return true;
+        }
+}
+
+
+void* ReadThread(void *arg)
+{
+	CMysql db;
+    	int clientfd = (int)arg;
+    	while(true)
+    	{
+        	int size = 0;
+      		char recvbuf[1024]={0};
+
+        	Json::Reader reader;
+        	Json::Value root;
+        	Json::Value response;
+
+                size = recv(clientfd, recvbuf, 1024, 0);
+                if (size <= 0)
+        	{
+                	cout<<"client connect fail!"<<errno<<endl;
+                	close(clientfd);
+                	return NULL;
+        	}
+
+                cout<<"recvbuf is  "<<recvbuf<<endl;
+                if (reader.parse(recvbuf, root))
+                {
+                	int msgtype = root["msgtype"].asInt();
+        	    	switch(msgtype)
+            		{
+                		case EN_MSG_LOGIN:
+                		{
+                        		response["msgtype"] = EN_MSG_ACK;
+                        		response["FD"] = root["FD"].asInt();
+
+                    			string name = root["name"].asString();
+                    			if(db.queryPasswd(name.c_str(), root["pwd"].asString().c_str()))
+                    			{
+                        			response["ackcode"] = "ok";
+                                        	if (!db.insertIntoStates(name.c_str(), root["FD"].asInt()))
+                        			{
+                                			cout<<"insertIntoStates error"<<endl;
+                        			}
+                    			}
+                    			else
+                    			{
+                        			response["ackcode"] = "error";
+                   		 	}
+                    			send(clientfd, response.toStyledString().c_str(),strlen(response.toStyledString().c_str())+1, 0);
+                    			sendMesgFromDb(db, root["FD"].asInt(), root["name"].asString().c_str(), clientfd);                
+				}
+                		break;
+
+                		case EN_MSG_CHAT://chat with other
+                		{
+                                        int tempfd = db.getStates(root["to"].asString().c_str());
+                                        cout<<"to "<<root["to"].asString().c_str()<<"  tempfd is  "<<tempfd<<endl;
+
+                                        if(tempfd != -1)
+                                        {
+                                                response["msgtype"] = EN_MSG_CHAT;
+                                                response["from"] = root["from"].asString();
+                                                response["msg"] = root["msg"].asString();
+                                                response["FD"] = tempfd;
+                                        }
+                                        else
+                                        {
+                                                insertIntoMessage(root["from"].asString().c_str(), root["to"].asString().c_str(), root["msg"].asString().c_str(), db);
+                                                response["FD"] = root["FD"].asInt();
+                                                response["msgtype"] = EN_MSG_ACK;
+                                                response["ackcode"] = "he isn't inline";
+                                        }
+                                        send(clientfd, response.toStyledString().c_str(), strlen(response.toStyledString().c_str())+1,0);
+                		}
+                		break;
+
+                		case EN_MSG_REGISTER:
+                                {
+                                        string name = root["name"].asString();
+                                        string passwd = root["passwd"].asString();
+                                        string email = root["email"].asString();
+
+                                        response["msgtype"] = EN_MSG_ACK;
+                                	response["FD"] = root["FD"].asInt();
+
+                                        if (!db.insertIntoUser(name.c_str(), passwd.c_str(), email.c_str()))
+                                        {
+                                                cout<<"do it fail"<<endl;
+                                                response["ackcode"] = "no";
+                                        }
+                                        else
+                                        {
+                                                response["ackcode"] = "yes";
+                                        }
+                                	send(clientfd, response.toStyledString().c_str(),strlen(response.toStyledString().c_str())+1, 0);
+                                }
+                        	break;
+
+                        	case EN_MSG_OFFLINE:
+                                {
+                                        close(clientfd);
+                                        delStateByfd(db, clientfd);//bool alter_state(int fd);
+                                        cout<<"server offlien"<<endl;
+                                }
+                                break;
+
+            }
+        }
+    }
+}
---
+++ lodeBalancer/server2.cpp
@@ -0,0 +1,47 @@
+//from server file--
+#include "head.h"
+
+int searchFd(CMysql &db, const char *name)
+{
+	if (name == NULL || strlen(name)>12)
+	{
+		return -1;
+	}
+	return db.getStates(name);//对离线信息进行存储，之后按照姓名进行查找
+}
+
+bool findMesgByName(CMysql &db, const char*name, char *str, int length)
+{
+	return db.findMessageByName(name, str, length);
+}
+
+bool delAllStateInfo(CMysql &db)//删除state表中的所有数据
+{
+	return db.deleteElemBySocket(0, 2);//需要通过对象调用
+}
+
+bool delStateByfd(CMysql &db, int fd)
+{
+	return db.deleteElemBySocket(1, fd);
+}
+
+bool insertIntoMessage(const char *fromuser, const char *touser, const char* msg, CMysql &db)
+{
+	return db.insertIntoMessage(fromuser, touser, msg);
+}
+
+bool delSendedMsg(CMysql &db, const char *name)
+{
+	return db.delInMessage(name);
+}
+//未完成函数
+//服务器停止之后只保留注册者user的信息，并且将所有的states中的信息都给删除
+//当从lb中受到客户端断开连接之后，将其的states信息进行修改删除
+
+
+//负载均衡器的功能
+//从lb接受消息，由于一台服务器只连接一个负载均衡器，所以对数据的收和发都是对同一个fd进行的
+//1.当客户端请求的是注册时，mysql中增加user,回复的信息中将发来的fd添加
+//2.当客户端是登陆的时候，向state表中插入数据，回复信息中增加其发来的fd
+//3.当客户端是聊天的时候，查询对方是否在线，如果在线就将此包中的fd更换，将消息进行转发，
+//  --如果不在线的话就将消息存储message<message，from, to>，向发送来的fd回复提醒消息
